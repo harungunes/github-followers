90fb458851eb75b6d20b6d63262811e716935524
harungunes <h.gunes13@gmail.com>
2021-12-14 20:21:02 +0100

    inite repo

---
+++ GHFollowers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GHFollowers
+//
+//  Created by Harun Gunes on 14/12/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
