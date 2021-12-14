ec0c500bf0fcf0e9b1eec30a3fbf0df234c0e380
harungunes <h.gunes13@gmail.com>
2021-12-14 21:47:45 +0100

    add FavoritesVC and SearchVC

---
+++ GHFollowers/SearchVC.swift
@@ -0,0 +1,16 @@
+//
+//  SearchVC.swift
+//  GHFollowers
+//
+//  Created by Harun Gunes on 14/12/2021.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .systemBlue
+  }
+}
