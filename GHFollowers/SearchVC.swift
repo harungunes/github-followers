144908300a88eb4d01ef66d967b3ab68fb2bad7a
harungunes <h.gunes13@gmail.com>
2021-12-14 23:29:44 +0100

    configure search button

--- GHFollowers/SearchVC.swift
+++ GHFollowers/SearchVC.swift
@@ -13,7 +13,7 @@ class SearchVC: UIViewController {
   
   let logoImageView = UIImageView()
   let usernameTextField = GFTextField()
-  let searchButton = GFButton(backgroungColor: .systemGreen, title: "Get Followers")
+  let callToActionButton = GFButton(backgroundColor: .systemGreen, title: "Get Followers")
   
   // MARK: - Lifecycle
   
@@ -22,6 +22,7 @@ class SearchVC: UIViewController {
     view.backgroundColor = .systemBackground
     configureLogoImageView()
     configureTextField()
+    configureCallToActionButton()
   }
   
   override func viewWillAppear(_ animated: Bool) {
@@ -52,6 +53,17 @@ class SearchVC: UIViewController {
       usernameTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
       usernameTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
       usernameTextField.heightAnchor.constraint(equalToConstant: 50)
+    ])
+  }
+  
+  func configureCallToActionButton() {
+    view.addSubview(callToActionButton)
+    
+    NSLayoutConstraint.activate([
+      callToActionButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -50),
+      callToActionButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
+      callToActionButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
+      callToActionButton.heightAnchor.constraint(equalToConstant: 50)
     ])
   }
 }
