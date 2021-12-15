02a8a820a10fce460c5d17ff824c2cf1f0d672d5
harungunes <h.gunes13@gmail.com>
2021-12-15 18:03:14 +0100

    modify navigation bar tint color in SceneDelegate

--- GHFollowers/SceneDelegate.swift
+++ GHFollowers/SceneDelegate.swift
@@ -20,7 +20,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     window?.windowScene = windowScene
     window?.rootViewController = createTabbar()
     window?.makeKeyAndVisible()
-    
+   
+    configureNavigationBar()
   }
   
   func createSearchNavigationController() -> UINavigationController {
@@ -47,6 +48,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     return tabbar
   }
 
+  func configureNavigationBar() {
+    UINavigationBar.appearance().tintColor = .systemGreen
+  }
 
   func sceneDidDisconnect(_ scene: UIScene) {
     // Called as the scene is being released by the system.
