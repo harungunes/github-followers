05c84db914564ecd56eba918007d91eae10a9a2f
harungunes <h.gunes13@gmail.com>
2021-12-18 23:54:01 +0100

    create Follower model

---
+++ GHFollowers/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GHFollowers
+//
+//  Created by Harun Gunes on 18/12/2021.
+//
+
+import Foundation
+
+struct Follower: Codable {
+  var login: String
+  var avatarUrl: String
+}
