d6560347e63f423418db2f4283744d0196d1b7f8
harungunes <h.gunes13@gmail.com>
2021-12-18 22:37:17 +0100

    add UIViewController extension to present custom alert on a main thread

---
+++ GHFollowers/UIViewConroller+Extension.swift
@@ -0,0 +1,19 @@
+//
+//  UIViewConroller+Extension.swift
+//  GHFollowers
+//
+//  Created by Harun Gunes on 18/12/2021.
+//
+
+import UIKit
+
+extension UIViewController {
+  func presentGFAlertOnMainThread(title: String, message: String, buttonTitle: String) {
+    DispatchQueue.main.async {
+      let alertVC = GFAlertVC(title: title, message: message, buttonTitle: buttonTitle)
+      alertVC.modalPresentationStyle = .overFullScreen
+      alertVC.modalTransitionStyle = .crossDissolve
+      self.present(alertVC, animated: true, completion: nil)
+    }
+  }
+}
