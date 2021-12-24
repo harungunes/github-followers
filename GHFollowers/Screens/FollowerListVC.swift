4915bc1e77e97bfec242fa76efc69a28826097d6
harungunes <h.gunes13@gmail.com>
2021-12-24 20:50:42 +0100

    apply Result type for success and failure cases

--- GHFollowers/Screens/FollowerListVC.swift
+++ GHFollowers/Screens/FollowerListVC.swift
@@ -15,14 +15,17 @@ class FollowerListVC: UIViewController {
     view.backgroundColor = .systemBackground
     navigationController?.navigationBar.prefersLargeTitles = true
     
-    NetworkManager.shared.getFollowers(for: username, page: 100) { (followers, errorMessage) in
-      guard let followers = followers else {
-        self.presentGFAlertOnMainThread(title: "Bad stuff happened here", message: errorMessage!.rawValue, buttonTitle: "Ok")
-        return
+    NetworkManager.shared.getFollowers(for: username, page: 1) { result in
+      
+      switch result {
+      case .success(let followers):
+        print("Followers.count = \(followers.count)")
+        print(followers)
+        
+      case .failure(let error):
+        self.presentGFAlertOnMainThread(title: "Bad stuff happened here", message: error.rawValue, buttonTitle: "Ok")
       }
       
-      print("Followers.count = \(followers.count)")
-      print(followers)
     }
   }
   
