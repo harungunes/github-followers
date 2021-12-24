e57c518ffa0d87d30a21ec5a35d90d04f6348f5e
harungunes <h.gunes13@gmail.com>
2021-12-24 21:31:47 +0100

    initialize the collection view in FollowerListVC

--- GHFollowers/Screens/FollowerListVC.swift
+++ GHFollowers/Screens/FollowerListVC.swift
@@ -9,11 +9,36 @@ import UIKit
 class FollowerListVC: UIViewController {
   
   var username: String!
+  var collectionView: UICollectionView!
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    
+    configure()
+    configureCollectionView()
+    getFollowers()
+    
+  }
+  
+  private func configure() {
     view.backgroundColor = .systemBackground
     navigationController?.navigationBar.prefersLargeTitles = true
+  }
+  
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    navigationController?.setNavigationBarHidden(false, animated: true)
+  }
+  
+  func configureCollectionView() {
+    collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UICollectionViewLayout())
+    view.addSubview(collectionView)
+    collectionView.translatesAutoresizingMaskIntoConstraints = false
+    collectionView.backgroundColor = .systemPink
+    collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
+  }
+  
+  func getFollowers() {
     
     NetworkManager.shared.getFollowers(for: username, page: 1) { result in
       
@@ -27,10 +52,5 @@ class FollowerListVC: UIViewController {
       }
       
     }
-  }
-  
-  override func viewWillAppear(_ animated: Bool) {
-    super.viewWillAppear(animated)
-    navigationController?.setNavigationBarHidden(false, animated: true)
   }
 }
