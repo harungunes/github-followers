4fd6b1fd26d72c25615f0394b168b942ce59654d
harungunes <h.gunes13@gmail.com>
2021-12-26 18:56:15 +0100

    apple diffable data source to the FollowerCell in FollowerListVC

--- GHFollowers/Screens/FollowerListVC.swift
+++ GHFollowers/Screens/FollowerListVC.swift
@@ -8,8 +8,15 @@ import UIKit
 
 class FollowerListVC: UIViewController {
   
+  enum Section {
+    case main
+  }
+  
   var username: String!
+  var followers: [Follower] = []
+  
   var collectionView: UICollectionView!
+  var dataSource: UICollectionViewDiffableDataSource<Section, Follower>!
   
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -17,7 +24,7 @@ class FollowerListVC: UIViewController {
     configure()
     configureCollectionView()
     getFollowers()
-    
+    configureDataSource()
   }
   
   private func configure() {
@@ -59,13 +66,28 @@ class FollowerListVC: UIViewController {
       
       switch result {
       case .success(let followers):
-        print("Followers.count = \(followers.count)")
-        print(followers)
+        self.followers = followers
+        self.updateData()
         
       case .failure(let error):
         self.presentGFAlertOnMainThread(title: "Bad stuff happened here", message: error.rawValue, buttonTitle: "Ok")
       }
       
     }
+  }
+  
+  func configureDataSource() {
+    dataSource = UICollectionViewDiffableDataSource<Section, Follower>(collectionView: collectionView, cellProvider: { (collectionView, indexPath, follower) -> UICollectionViewCell? in
+      let cell = collectionView.dequeueReusableCell(withReuseIdentifier: FollowerCell.reuseID, for: indexPath) as! FollowerCell
+      cell.set(follower: follower)
+      return cell
+    })
+  }
+  
+  func updateData() {
+    var snapshot = NSDiffableDataSourceSnapshot<Section, Follower>()
+    snapshot.appendSections([.main])
+    snapshot.appendItems(followers)
+    DispatchQueue.main.async { self.dataSource.apply(snapshot, animatingDifferences: true) }
   }
 }
