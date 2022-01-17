fa410441b7855c57b529796e213c2678fdf5547e
harungunes <h.gunes13@gmail.com>
2022-01-17 21:38:22 +0100

    create UserInfoVC and present it as follower cell is clicked

---
+++ GHFollowers/Screens/UserInfoVC.swift
@@ -0,0 +1,28 @@
+//
+//  UserInfoVC.swift
+//  GHFollowers
+//
+//  Created by Harun Gunes on 17/01/2022.
+//
+
+import UIKit
+
+class UserInfoVC: UIViewController {
+  
+  var username: String!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    view.backgroundColor = .systemBackground
+    let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissVC))
+    navigationItem.rightBarButtonItem = doneButton
+    
+  }
+  
+  @objc
+  func dismissVC() {
+    dismiss(animated: true)
+  }
+  
+}
