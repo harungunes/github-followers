6fe5cb9fcde8313ade7537c54bc5f076d26c6a49
harungunes <h.gunes13@gmail.com>
2022-01-20 20:58:14 +0100

    create a network call in UserInfoVC

--- GHFollowers/Screens/UserInfoVC.swift
+++ GHFollowers/Screens/UserInfoVC.swift
@@ -18,6 +18,16 @@ class UserInfoVC: UIViewController {
     let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissVC))
     navigationItem.rightBarButtonItem = doneButton
 
+    NetworkManager.shared.getUserInfo(for: username) { [weak self] result in
+      guard let self = self else { return }
+      
+      switch result {
+      case .failure(let error):
+        self.presentGFAlertOnMainThread(title: "Something went wrong", message: error.rawValue, buttonTitle: "Ok")
+      case .success(let user):
+        print(user)
+      }
+    }
   }
   
   @objc
