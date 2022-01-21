1e50e991bc9218a43ad91e82d0c47d5d4a3494a0
harungunes <h.gunes13@gmail.com>
2022-01-21 18:15:15 +0100

    create a child view controller called GFUserInfoVC

---
+++ GHFollowers/Custom Views/ViewControllers/GFUserInfoVC.swift
@@ -0,0 +1,15 @@
+//
+//  GFUserInfoVC.swift
+//  GHFollowers
+//
+//  Created by Harun Gunes on 21/01/2022.
+//
+
+import UIKit
+
+class GFUserInfoVC: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
