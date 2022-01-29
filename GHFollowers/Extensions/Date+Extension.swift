e597f59900cdaedca0c8c9dfbc800cb05d84e8f2
harungunes <h.gunes13@gmail.com>
2022-01-29 14:04:30 +0100

    modify date and string extensions to display date in different format

--- GHFollowers/Extensions/Date+Extension.swift
+++ GHFollowers/Extensions/Date+Extension.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+
+extension Date {
+  
+  func convertToMonthYearFormat() -> String {
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "MMM yyyy"
+    
+    return dateFormatter.string(from: self)
+  }
+}
--- GHFollowers/Screens/UserInfoVC.swift
+++ GHFollowers/Screens/UserInfoVC.swift
@@ -12,6 +12,7 @@ class UserInfoVC: UIViewController {
   let headerView = UIView()
   let itemViewOne = UIView()
   let itemViewTwo = UIView()
+  let dateLabel = GFBodyLabel(textAlignment: .center )
   
   var itemViews: [UIView] = []
   
@@ -42,6 +43,7 @@ class UserInfoVC: UIViewController {
           self.add(childVC: GFUserInfoHeaderVC(user: user), to: self.headerView)
           self.add(childVC: GFRepoItemVC(user: user), to: self.itemViewOne)
           self.add(childVC: GFFollowerItemVC(user: user), to: self.itemViewTwo)
+          self.dateLabel.text = "sdfuihdsfg"
         }
       }
     }
@@ -51,7 +53,7 @@ class UserInfoVC: UIViewController {
     let padding: CGFloat = 20
     let itemHeight: CGFloat = 140
     
-    itemViews = [headerView, itemViewOne, itemViewTwo]
+    itemViews = [headerView, itemViewOne, itemViewTwo, dateLabel]
     
     for item in itemViews {
       view.addSubview(item)
@@ -73,6 +75,9 @@ class UserInfoVC: UIViewController {
       
       itemViewTwo.topAnchor.constraint(equalTo: itemViewOne.bottomAnchor, constant: padding),
       itemViewTwo.heightAnchor.constraint(equalToConstant: itemHeight),
+      
+      dateLabel.topAnchor.constraint(equalTo: itemViewTwo.bottomAnchor, constant: padding),
+      dateLabel.heightAnchor.constraint(equalToConstant: 18)
     ])
   }
   
