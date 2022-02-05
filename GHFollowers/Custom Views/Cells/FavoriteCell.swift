0c7b14af92828c53043ae3980ce7b44512f930ca
harungunes <h.gunes13@gmail.com>
2022-02-05 15:40:01 +0100

    add constraints and set function to table view cell

--- GHFollowers/Custom Views/Cells/FavoriteCell.swift
+++ GHFollowers/Custom Views/Cells/FavoriteCell.swift
@@ -15,11 +15,36 @@ class FavoriteCell: UITableViewCell {
   
   override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
     super.init(style: style, reuseIdentifier: reuseIdentifier)
+    configure()
   }
   
   required init?(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
   }
   
+  func set(favorite: Follower) {
+    usernameLabel.text = favorite.login
+    avatarImageView.downloadImage(from: favorite.avatarUrl)
+  }
   
+  private func configure() {
+    addSubview(avatarImageView)
+    addSubview(usernameLabel)
+    
+    accessoryType = .disclosureIndicator
+    
+    let padding: CGFloat = 12
+    
+    NSLayoutConstraint.activate([
+      avatarImageView.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+      avatarImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: padding),
+      avatarImageView.heightAnchor.constraint(equalToConstant: 60),
+      avatarImageView.widthAnchor.constraint(equalToConstant: 60),
+      
+      usernameLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+      avatarImageView.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor, constant: 24),
+      usernameLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -padding),
+      usernameLabel.heightAnchor.constraint(equalToConstant: 40)
+    ])
+  }
 }
